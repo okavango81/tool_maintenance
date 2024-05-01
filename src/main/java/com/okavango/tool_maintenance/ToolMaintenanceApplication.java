5646e47d8dc7ea46e373b41e1e6b1a3aaae8f301
okavango81 <eguilhermeleitedev81@gmail.com>
2024-04-30 21:02:39 -0300

    created project

---
+++ src/main/java/com/okavango/tool_maintenance/ToolMaintenanceApplication.java
@@ -0,0 +1,13 @@
+package com.okavango.tool_maintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolMaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolMaintenanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/okavango/tool_maintenance/ToolMaintenanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okavango.tool_maintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolMaintenanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
